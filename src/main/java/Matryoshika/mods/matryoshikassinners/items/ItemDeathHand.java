5fe5fd582cf9103d5f9a0d7881992cbeac9db734
Matryoshika <mad.matryoshika@gmail.com>
2015-09-06 13:36:23 +0200

    Hand of Death

---
+++ src/main/java/Matryoshika/mods/matryoshikassinners/items/ItemDeathHand.java
@@ -0,0 +1,136 @@
+package Matryoshika.mods.matryoshikassinners.items;
+
+import java.util.List;
+
+import Matryoshika.mods.matryoshikassinners.matryoshikassinners;
+import Matryoshika.mods.matryoshikassinners.blocks.altars.PaganAltar;
+import net.minecraft.item.ItemSword;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.boss.IBossDisplayData;
+import net.minecraft.entity.effect.EntityLightningBolt;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.monster.*;
+import net.minecraft.entity.passive.*;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemStack;
+
+public class ItemDeathHand extends ItemSword {
+	
+	int bloodAmount = 1;
+	int soulFragment = 0;
+	
+	public ItemDeathHand(ToolMaterial sacrifice) {
+		super(sacrifice);
+		this.maxStackSize = 1;
+		
+		this.setUnlocalizedName("ItemDeathHand");
+		this.setTextureName(matryoshikassinners.MODID+":DeathHand");
+	}
+	
+	@Override
+	public void addInformation(ItemStack stack1, EntityPlayer player, List list, boolean par){
+		list.add(StatCollector.translateToLocal("lore.deathhand.desc1"));
+	}
+	
+	@Override
+	public boolean hitEntity(ItemStack stack, EntityLivingBase base1, EntityLivingBase base2){
+		
+		
+		if (base2 == null || base1 == null || base2.worldObj.isRemote){
+			return false;
+		}
+		if (base1 instanceof EntityPlayer || base1 instanceof IBossDisplayData)
+        {
+            return false;
+        }
+		
+		World world = base1.worldObj;
+		if (base1.isDead || base1.getHealth() < 0.5f)
+        {
+            return false;
+        }
+		
+		if (base1 instanceof EntityVillager) soulFragment = 1;
+		else if (base1 instanceof EntityZombie) soulFragment = 2;
+		else if (base1 instanceof EntityAnimal) soulFragment = 3;
+		else if (base1 instanceof EntityGhast || base1 instanceof EntityCreeper) soulFragment = 4;
+		
+		if (findPaganAltar(base1.worldObj, base1, soulFragment))
+        {
+            double posX = base1.posX;
+            double posY = base1.posY;
+            double posZ = base1.posZ;
+            
+            base1.setHealth(-1);
+            base1.worldObj.addWeatherEffect(new EntityLightningBolt(base1.worldObj, posX, posY, posZ));
+            base1.onDeath(DamageSource.generic);
+            
+            if(soulFragment == 1){
+            	ItemStack soul = new ItemStack(matryoshikassinners_Items.VillagerSoul,1,0);
+            	Entity item = new EntityItem(base1.worldObj, posX, posY, posZ, soul);
+            	base1.worldObj.spawnEntityInWorld(item);
+            }
+            if(soulFragment == 2){
+            	ItemStack soul = new ItemStack(matryoshikassinners_Items.ZombieSoul,1,0);
+            	Entity item = new EntityItem(base1.worldObj, posX, posY, posZ, soul);
+            	base1.worldObj.spawnEntityInWorld(item);
+            }
+            if(soulFragment == 3){
+            	ItemStack soul = new ItemStack(matryoshikassinners_Items.AnimalSoul,1,0);
+            	Entity item = new EntityItem(base1.worldObj, posX, posY, posZ, soul);
+            	base1.worldObj.spawnEntityInWorld(item);
+            }
+            if(soulFragment == 4){
+            	ItemStack soul = new ItemStack(matryoshikassinners_Items.BuffMobSoul,1,0);
+            	Entity item = new EntityItem(base1.worldObj, posX, posY, posZ, soul);
+            	base1.worldObj.spawnEntityInWorld(item);
+            }
+            
+            
+            
+            
+        }
+		return false;
+		
+	}
+	 public boolean findPaganAltar(World world, EntityLivingBase sacrifice, int amount)
+	    {
+	        int posX = (int) Math.round(sacrifice.posX - 0.5f);
+	        int posY = (int) sacrifice.posY;
+	        int posZ = (int) Math.round(sacrifice.posZ - 0.5f);
+	        PaganAltar altarEntity = this.getAltar(world, posX, posY, posZ);
+
+	        if (altarEntity == null)
+	        {
+	            return false;
+	        }
+	        return true;
+	    }
+	 public PaganAltar getAltar(World world, int x, int y, int z){
+	        Block block;
+
+	        for (int i = -2; i <= 2; i++){
+	            for (int j = -2; j <= 2; j++){
+	                for (int k = -2; k <= 1; k++){
+	                    block = world.getBlock(i + x, k + y, j + z);
+
+	                    if ((block instanceof PaganAltar)){
+	                        return (PaganAltar) block;
+	                    }
+	                }
+	            }
+	        }
+
+	        return null;
+	 }
+}
