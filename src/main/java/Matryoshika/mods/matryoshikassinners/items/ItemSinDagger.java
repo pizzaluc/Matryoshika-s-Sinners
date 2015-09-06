69f8270b6255827e443a3548a67de9ea9b52df6d
Matryoshika <mad.matryoshika@gmail.com>
2015-09-06 13:36:40 +0200

    Sin weapon; tiny version

---
+++ src/main/java/Matryoshika/mods/matryoshikassinners/items/ItemSinDagger.java
@@ -0,0 +1,17 @@
+package Matryoshika.mods.matryoshikassinners.items;
+
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSword;
+import Matryoshika.mods.matryoshikassinners.matryoshikassinners;
+
+public class ItemSinDagger extends ItemSword {
+	
+	public ItemSinDagger(ToolMaterial sin) {
+		super(sin);
+		
+		this.setUnlocalizedName("ItemSinDagger");
+		this.setTextureName(matryoshikassinners.MODID+":dagger");
+	}
+
+	
+}
