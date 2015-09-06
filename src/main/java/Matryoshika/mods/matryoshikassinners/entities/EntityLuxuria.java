80e7deb14bb45df64193d7e2e7e2d264d7501d25
Matryoshika <mad.matryoshika@gmail.com>
2015-09-06 13:34:48 +0200

    7 Deadly bosses

---
+++ src/main/java/Matryoshika/mods/matryoshikassinners/entities/EntityLuxuria.java
@@ -0,0 +1,72 @@
+package Matryoshika.mods.matryoshikassinners.entities;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILeapAtTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.boss.BossStatus;
+import net.minecraft.entity.boss.IBossDisplayData;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.world.World;
+
+
+public class EntityLuxuria extends EntityMob implements IBossDisplayData{
+	
+	private static final float MAX_HP = 300F;
+	
+	public EntityLuxuria(World world)
+    {
+        super(world);
+        setSize(1,2);
+        this.isImmuneToFire = true;
+        preventEntitySpawning = true;
+        getNavigator().setCanSwim(true);
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIAttackOnCollide(this, EntityPlayer.class, 1.0D, false));
+        this.tasks.addTask(2, new EntityAIMoveTowardsRestriction(this, 1.0D));
+        this.tasks.addTask(3, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(4, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
+    }
+	
+	@Override
+	protected boolean canDespawn() {
+		return false;
+	}
+	
+	public EntityLuxuria(World world, double x, double y, double z){
+		this(world);
+		setPosition(x,y,z);		
+	}
+	
+	
+	@Override
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.4);
+		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(MAX_HP);
+		this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(1.0);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(100D);
+	}
+	
+	public boolean isAIEnabled(){
+
+		return true;
+		}
+}
