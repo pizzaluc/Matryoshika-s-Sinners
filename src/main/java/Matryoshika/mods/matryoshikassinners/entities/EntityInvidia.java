304061f92804bfdb7b0313c738328b7762092995
Matryoshika <mad.matryoshika@gmail.com>
2015-09-07 19:49:11 +0200

    Invidia AI

--- src/main/java/Matryoshika/mods/matryoshikassinners/entities/EntityInvidia.java
+++ src/main/java/Matryoshika/mods/matryoshikassinners/entities/EntityInvidia.java
@@ -1,5 +1,8 @@
 package Matryoshika.mods.matryoshikassinners.entities;
 
+import java.util.List;
+
+import Matryoshika.mods.matryoshikassinners.items.matryoshikassinners_Items;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.Entity;
@@ -15,10 +18,15 @@ import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.boss.BossStatus;
 import net.minecraft.entity.boss.IBossDisplayData;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChatStyle;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.IChatComponent;
@@ -27,7 +35,8 @@ import net.minecraft.world.World;
 
 public class EntityInvidia extends EntityMob implements IBossDisplayData{
 	
-	private static final float MAX_HP = 300F;
+	private static final float MAX_HP = 500F;
+	private static final double RANGE = 20F;
 	
 	public EntityInvidia(World world)
     {
@@ -46,6 +55,19 @@ public class EntityInvidia extends EntityMob implements IBossDisplayData{
     }
 	
 	@Override
+	public void onEntityUpdate(){
+		copyPlayer(this);
+		super.onEntityUpdate();
+	}
+	@Override
+	public void onDeath(DamageSource source){
+		for(int i = 0; i<5; i++){
+		this.equipmentDropChances[i] = -1;
+		}
+		super.onDeath(source);
+	}
+	
+	@Override
 	protected boolean canDespawn() {
 		return false;
 	}
@@ -59,14 +81,43 @@ public class EntityInvidia extends EntityMob implements IBossDisplayData{
 	@Override
 	protected void applyEntityAttributes() {
 		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.4);
+		this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.6);
 		this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(MAX_HP);
 		this.getEntityAttribute(SharedMonsterAttributes.knockbackResistance).setBaseValue(1.0);
-		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(100D);
+		this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(10D);
 	}
 	
 	public boolean isAIEnabled(){
 
 		return true;
 		}
+	
+	private void copyPlayer(Entity EntityInvidia){
+		AxisAlignedBB box = EntityInvidia.boundingBox.expand(RANGE, RANGE, RANGE);
+		Class<EntityPlayer> player = EntityPlayer.class;
+		List<EntityPlayer> inbox = EntityInvidia.worldObj.getEntitiesWithinAABB(player, box);
+		
+		for (EntityPlayer entityplayer : inbox) {
+			
+			if(entityplayer.getCurrentArmor(3) != null){
+				ItemStack helm = entityplayer.getCurrentArmor(3).copy();
+				EntityInvidia.setCurrentItemOrArmor(4, helm);
+			}
+			if(entityplayer.getCurrentArmor(2) != null){
+				ItemStack torso = entityplayer.getCurrentArmor(2).copy();
+				EntityInvidia.setCurrentItemOrArmor(3, torso);
+			}
+			if(entityplayer.getCurrentArmor(1) != null){
+				ItemStack leggings = entityplayer.getCurrentArmor(1).copy();
+				EntityInvidia.setCurrentItemOrArmor(2, leggings);
+			}
+			if(entityplayer.getCurrentArmor(0) != null){
+				ItemStack boots = entityplayer.getCurrentArmor(0).copy();
+				EntityInvidia.setCurrentItemOrArmor(1, boots);
+			}
+			ItemStack weapon = new ItemStack(matryoshikassinners_Items.SinDagger);
+			EntityInvidia.setCurrentItemOrArmor(0, weapon);
+		}
+		
+	}
 }
