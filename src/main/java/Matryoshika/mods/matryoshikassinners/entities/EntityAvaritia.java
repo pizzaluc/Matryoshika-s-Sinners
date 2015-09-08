9c3c90e19415ffe710a83ac273bee3249b4ebe09
Matryoshika <mad.matryoshika@gmail.com>
2015-09-08 02:39:14 +0200

    Avaritia AI

--- src/main/java/Matryoshika/mods/matryoshikassinners/entities/EntityAvaritia.java
+++ src/main/java/Matryoshika/mods/matryoshikassinners/entities/EntityAvaritia.java
@@ -1,5 +1,8 @@
 package Matryoshika.mods.matryoshikassinners.entities;
 
+import java.util.List;
+import java.util.Random;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.Entity;
@@ -15,8 +18,12 @@ import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.boss.BossStatus;
 import net.minecraft.entity.boss.IBossDisplayData;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
@@ -28,9 +35,10 @@ import net.minecraft.world.World;
 public class EntityAvaritia extends EntityMob implements IBossDisplayData{
 	
 	private static final float MAX_HP = 300F;
+	private static final double RANGE = 5F;
+	private int counter;
 	
-	public EntityAvaritia(World world)
-    {
+	public EntityAvaritia(World world){
         super(world);
         setSize(1,2);
         this.isImmuneToFire = true;
@@ -44,6 +52,12 @@ public class EntityAvaritia extends EntityMob implements IBossDisplayData{
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
         this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityPlayer.class, 0, true));
     }
+	@Override
+	public void onEntityUpdate(){
+		counter++;
+		stealItems(this);
+		super.onEntityUpdate();
+	}
 	
 	@Override
 	protected boolean canDespawn() {
@@ -69,4 +83,47 @@ public class EntityAvaritia extends EntityMob implements IBossDisplayData{
 
 		return true;
 		}
+	
+	private void stealItems(Entity EntityAvaritia){
+		AxisAlignedBB box = EntityAvaritia.boundingBox.expand(RANGE, RANGE, RANGE);
+		Class<EntityPlayer> players = EntityPlayer.class;
+		
+		List<EntityPlayer> inbox = EntityAvaritia.worldObj.getEntitiesWithinAABB(players, box);
+		
+			for (EntityPlayer entityPlayer : inbox) {
+				Random rand1 = new Random();
+				int random1 = rand1.nextInt(32) + 1;
+				Random rand2 = new Random();
+				int random2 = rand2.nextInt(5) + 1;
+				
+				if(random1 == 1 && counter >= 20 && !worldObj.isRemote){
+					if(random2 == 1 && entityPlayer.getCurrentArmor(0) != null){
+						ItemStack helm = entityPlayer.getHeldItem().copy();
+						entityPlayer.setCurrentItemOrArmor(1, null);
+						entityPlayer.entityDropItem(helm, 1);
+					}
+					if(random2 == 2 && entityPlayer.getCurrentArmor(1) != null){
+						ItemStack torso = entityPlayer.getHeldItem().copy();
+						entityPlayer.setCurrentItemOrArmor(2, null);
+						entityPlayer.entityDropItem(torso, 1);
+					}
+					if(random2 == 3 && entityPlayer.getCurrentArmor(2) != null){
+						ItemStack leggings = entityPlayer.getHeldItem().copy();
+						entityPlayer.setCurrentItemOrArmor(3, null);
+						entityPlayer.entityDropItem(leggings, 1);
+					}
+					if(random2 == 4 && entityPlayer.getCurrentArmor(3) != null){
+						ItemStack boots = entityPlayer.getHeldItem().copy();
+						entityPlayer.setCurrentItemOrArmor(4, null);
+						entityPlayer.entityDropItem(boots, 1);
+					}
+					if(random2 == 5 && entityPlayer.getHeldItem() != null){
+						ItemStack hand = entityPlayer.getHeldItem().copy();
+						entityPlayer.setCurrentItemOrArmor(0, null);
+						entityPlayer.entityDropItem(hand, 1);
+					}
+					counter = 0;
+				}
+			}
+	}
 }
