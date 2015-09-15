240492afd08a379b1cdd5694e003b8749c10ab5e
Matryoshika <mad.matryoshika@gmail.com>
2015-09-15 05:18:06 +0200

    Updating Main

--- src/main/java/Matryoshika/mods/matryoshikassinners/matryoshikassinners.java
+++ src/main/java/Matryoshika/mods/matryoshikassinners/matryoshikassinners.java
@@ -1,5 +1,6 @@
 package Matryoshika.mods.matryoshikassinners;
 
+import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
@@ -14,16 +15,21 @@ import Matryoshika.mods.matryoshikassinners.entities.EntityLuxuria;
 import Matryoshika.mods.matryoshikassinners.entities.EntitySuperbia;
 import Matryoshika.mods.matryoshikassinners.entities.matryoshikassinners_Entities;
 import Matryoshika.mods.matryoshikassinners.items.matryoshikassinners_Items;
+import Matryoshika.mods.matryoshikassinners.rendering.GUIHandler.MSGuiHandler;
+import Matryoshika.mods.matryoshikassinners.utils.ConfigHandler;
 import Matryoshika.mods.matryoshikassinners.utils.CreativeTabMatryoshika;
+import Matryoshika.mods.matryoshikassinners.utils.MatryoshikasSinnersGeneratorList;
 import Matryoshika.mods.matryoshikassinners.utils.SinnersDelight;
 import Matryoshika.mods.matryoshikassinners.utils.matryoshikaEventHandler;
 import Matryoshika.mods.matryoshikassinners.worldgen.worldGenAltar;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
@@ -32,6 +38,7 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.potion.Potion;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 
 	@Mod(modid=matryoshikassinners.MODID, version=matryoshikassinners.VERSION, name="matryoshikassinners")
 		public class matryoshikassinners {	
@@ -47,7 +54,7 @@ import net.minecraftforge.common.MinecraftForge;
 		}
 	};
 		
-	@Mod.Instance
+	@Instance("matryoshikassinners")
 	public static matryoshikassinners instance;
 	
 	public static final Enchantment purify = new EnchantmentPurify(83, 1);
@@ -65,6 +72,8 @@ import net.minecraftforge.common.MinecraftForge;
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event){
+		ConfigHandler.init(new Configuration(event.getSuggestedConfigurationFile()));
+		MatryoshikasSinnersGeneratorList.init(new File(event.getModConfigurationDirectory(), "matryoshikassinnersGenerators.cfg"));
 		matryoshikassinners_Items.registerItems();
 		matryoshikassinners_Blocks.registerBlocks();
 		
@@ -90,13 +99,18 @@ import net.minecraftforge.common.MinecraftForge;
 		
 		potionsSinnersDelight = new SinnersDelight(28, true, 0).setIconIndex(0,0).setPotionName("Sinners Delight");
 
-		GameRegistry.registerTileEntity(Matryoshika.mods.matryoshikassinners.tile.TileEntityMatryoshikaBrain.class, "MatryoshikaBrain");
+		GameRegistry.registerTileEntity(Matryoshika.mods.matryoshikassinners.tile.generator.TileGenerator.class, "DynamicGenerator");
 		
 		GameRegistry.registerWorldGenerator(worldGen, 33);
+		
+		NetworkRegistry.INSTANCE.registerGuiHandler(this, new MSGuiHandler());
+
 	}
 	
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event){
+		
+		
 		
 	}
 }
