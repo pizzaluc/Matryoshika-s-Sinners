c8d9185e3d531a89a0061f5dd98ce4481b3d850c
Matryoshika <mad.matryoshika@gmail.com>
2015-09-15 05:17:25 +0200

    Some commonproxy code

--- src/main/java/Matryoshika/mods/matryoshikassinners/CommonProxy.java
+++ src/main/java/Matryoshika/mods/matryoshikassinners/CommonProxy.java
@@ -1,5 +1,13 @@
 package Matryoshika.mods.matryoshikassinners;
 
+import Matryoshika.mods.matryoshikassinners.rendering.GUI.GuiDynamicGenerator;
+import Matryoshika.mods.matryoshikassinners.rendering.GUIHandler.MSGuiHandler;
+import Matryoshika.mods.matryoshikassinners.utils.ConfigHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.IGuiHandler;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import net.minecraftforge.common.config.Configuration;
+
 public class CommonProxy {
 
 	public void registerRenderers(){
@@ -7,6 +15,22 @@ public class CommonProxy {
 	}
 	
 	public void registerEntities(){
+		
+	}
+	
+	public void initCommon() {
+	       NetworkRegistry.INSTANCE.registerGuiHandler(matryoshikassinners.MODID, new MSGuiHandler());
+	}
+
+	public void initClient() {
+
+	}
+
+	public void initServer() {
+
+	}
+	
+	public void preInit(FMLPreInitializationEvent event){
 		
 	}
 	
